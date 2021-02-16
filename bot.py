1d3423eacb74ef887fcbf622e3a220261f93f30b
시혁 박 <exon.indiv@gmail.com>
2021-02-16 18:09:51 +0900

    bot

--- bot.py
+++ bot.py
@@ -1,3 +1,11 @@
 import discord
+import json
 
+token = ''
+with open('C:\\settings.json', 'r') as f:
+    token = json.load(f)["bot_token"]
 client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'[*] {client.user} has connected to Discord!')
