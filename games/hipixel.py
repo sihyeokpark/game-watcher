01440bb29f6aed1a99da90e70666d20485a9ff99
시혁 박 <exon.indiv@gmail.com>
2021-02-16 20:27:13 +0900

    settings.json

--- games/hipixel.py
+++ games/hipixel.py
@@ -1,13 +1,17 @@
 import requests
 import json
 
-key = 'edc63735-7a78-4e6a-a509-54c7cab3badc'
-player_uuid = 'eeab4c6642ba48a9a3995ee593422b02'
+key = ''
+player_uuid = ''
+with open('../settings.json', 'r') as f:
+    hipixel_settings = json.load(f)["hipixel"]
+    key = hipixel_settings["key"]
+    player_uuid = hipixel_settings["player_uuid"]
 url = 'https://api.hypixel.net/status?key=' + key + '&uuid=' + player_uuid
 
 html = requests.get(url)
 result = json.loads(html.text)
 
-if result['success']: 
-    print('온라인')
+print(result)
+if result['session']['online']: print('온라인')
 else: print('오프라인')
