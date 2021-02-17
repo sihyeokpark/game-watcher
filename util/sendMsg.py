95f5b47e78f7599568750133ff14e1385b94f28c
시혁 박 <exon.indiv@gmail.com>
2021-02-17 15:33:10 +0900

    python => ts

--- util/sendMsg.py
+++ util/sendMsg.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import json
-sys.path.append("..")
-import bot
+from ..bot import client
 
 channel_id = ''
 with open('../settings.json', 'r') as f:
     channel_id = json.load(f)["channel_id"]
+
+if __name__ == "__main__":
+    print('start')
