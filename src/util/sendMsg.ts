77f34bd23f6871b6d48c9f3a2a7cdd3d7d655c51
시혁 박 <exon.indiv@gmail.com>
2021-02-20 17:37:39 +0900

    sendMsg

--- src/util/sendMsg.ts
+++ src/util/sendMsg.ts
@@ -2,7 +2,7 @@ import { Client, MessageEmbed } from "discord.js"
 
 export default function sendMsg(content: string | MessageEmbed, client: any, channel_id: string): void {
     try {
-        client?.channels?.cache?.get(channel_id).send(content)
+        client?.channels?.cache?.get(channel_id)?.send(content)
     } catch(e) {
         console.log(e)
     }
